294cc543409e583d5af6a430664652e5c49c98ca
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-06-26 10:54:22 +0300

    tests

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+from .test_pokemon_router import *
+from .test_trainer_router import *
+from .test_evolve_router import *
---
+++ tests/test_trainer_router.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from pokemon_api.tables_server.tables_server import server
+
+client = TestClient(server)
+
+
+def test_get_trainers_by_pokemon_success():
+    response = client.get("/trainers?pokemon_name=charmander")
+
+    # Check response status code
+    assert response.status_code == 200
+
+    # Check response JSON content
+    assert response.json() == ["Giovanni", "Jasmine", "Whitney"]
+
+
+def test_get_trainers_by_pokemon_not_found():
+    response = client.get("/trainers?pokemon_name=MyPokemon")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "MyPokemon pokemon not found."}
+
